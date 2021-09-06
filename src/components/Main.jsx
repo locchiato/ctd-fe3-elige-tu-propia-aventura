b17f131bc4d85411dcfd05dc2970a54df549b1a7
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-09-06 15:40:43 -0300

    Se muestra el primer dato desde el archivo json

---
+++ src/components/Main.jsx
@@ -0,0 +1,73 @@
+import data from "./data";
+import React, { Component } from "react";
+//import Opciones from "./Opciones";
+//import Recordatorio from "./Recordatorio";
+
+const historial = [];
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+      seleccionPrevia: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contador !== this.state.contador) {
+      historial.push(this.state.seleccionPrevia);
+    }
+  }
+/* 
+  handleClick = (e) => {
+    const id = e.target.id;
+    if (this.state.contador >= 7) {
+      alert("Fin.");
+    } else if (id === "A" && this.state.seleccionPrevia !== "A") {
+      this.setState({
+        contador: this.state.contador + 1,
+        seleccionPrevia: "A",
+      });
+    } else if (id === "A" && this.state.seleccionPrevia === "A") {
+      this.setState({
+        contador: this.state.contador + 2,
+      });
+    } else if (id === "B" && this.state.seleccionPrevia === "A") {
+      this.setState({
+        contador: this.state.contador + 3,
+        seleccionPrevia: "B",
+      });
+    } else if (id === "B") {
+      this.setState({
+        contador: this.state.contador + 2,
+        seleccionPrevia: "B",
+      });
+    }
+    console.log(historial);
+  }; */
+
+  render() {
+    return (
+      <div className="layout">
+        <h1 className="historia">{data[this.state.contador].historia}</h1>
+        {/* <Opciones
+          handleClick={this.handleClick}
+          opcionA={data[this.state.contador].opciones.a}
+          opcionB={data[this.state.contador].opciones.b}
+        />
+        <Recordatorio
+          seleccionPrevia={this.state.seleccionPrevia}
+          historial={historial.map(
+            (e, index) => (
+              <li key={index}>{e}</li>
+            ),
+            data[this.state.contador].id
+          )}
+        /> */}
+      </div>
+    );
+  }
+}
+
+export default Main;
