a303a1f9cc6acca1e430b704d610aaf7e45cebef
Leandro Occhiato <38090282+locchiato@users.noreply.github.com>
2021-09-08 02:37:11 -0300

    [MOD] Se cambiam fuentes, y se muestra historial

--- src/components/Recordatorio.jsx
+++ src/components/Recordatorio.jsx
@@ -2,9 +2,14 @@ import { Component } from "react";
 
 class Recordatorio extends Component {
   render() {
+    console.log(this.props.historial);
+
     return (
       <div className="recordatorio">
-        <div>{this.props.seleccionPrevia}</div>
+        <h3>Seleccion previa: {this.props.seleccionPrevia}</h3>
+        <hr />
+        <h4>Historial:</h4>
+        <ul>{this.props.historial}</ul>
       </div>
     );
   }
