79e81027de6155b414744e764ffa7468565d4664
Leandro Occhiato <38090282+locchiato@users.noreply.github.com>
2021-09-09 23:14:11 -0300

    [MOD] Se agregan acentos

--- src/components/Recordatorio.jsx
+++ src/components/Recordatorio.jsx
@@ -4,7 +4,7 @@ class Recordatorio extends Component {
   render() {
     return (
       <div className="recordatorio">
-        <h3>Ultima seleccion: {this.props.ultimaSeleccion}</h3>
+        <h3>Última selección: {this.props.ultimaSeleccion}</h3>
         <hr />
         <h4>Historial:</h4>
         <ul>{this.props.historial}</ul>
